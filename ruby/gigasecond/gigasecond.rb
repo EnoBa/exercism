ce31cd37d78f837001580b4b155797e2e211b83d
Enis Kahrimanovic <enis.kahrimanovic@sogeti.com>
2016-10-22 22:15:06 +0200

    Initial

--- ruby/gigasecond/gigasecond.rb
+++
@@ -1,9 +0,0 @@
-module BookKeeping
-  VERSION = 4 # Where the version number matches the one in the test.
-end
-
-class Gigasecond
-  def self.from(a)
-    return a + 1000000000
-  end
-end
--- ruby/hello-world/hello_world.rb
+++
@@ -1,7 +0,0 @@
-class HelloWorld
-
-  def self.hello (name = 'World')
-    return "Hello, #{name}!"
-  end
-
-end
